b5bb899a72e2a50a92265b599fd760b88eeeff6c
thinj <henrik.jaeger@gmail.com>
2022-09-26 04:08:24 +0200

    Indent .h file

--- buttons.h
+++ buttons.h
@@ -1,15 +1,15 @@
 #ifndef BUTTONS_H
 #define BUTTONS_H
-class Buttons
-{
-   public:
-      Buttons(int common, int up, int down);
-      void setup();
-      void loop(bool update);
-      int get();
-   private:
-      int aCommon;
-      int aUp;
-      int aDown;
-};
+  class Buttons
+  {
+     public:
+        Buttons(int common, int up, int down);
+        void setup();
+        void loop(bool update);
+        int get();
+     private:
+        int aCommon;
+        int aUp;
+        int aDown;
+  };
 #endif // BUTTONS_H
